29e0b9de4a7485cfa3902fc4f44ec81df859b5a1
Akash NP <akashnp1998@gmail.com>
2023-11-18 19:36:34 +0400

    chore: initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>Welcome to Splitwiser</h1>
+    </>
+  )
+}
+
+export default App
