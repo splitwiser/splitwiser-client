66f5e57f91ec47d7f3717839221548ee45c6a434
Akash NP <akashnp1998@gmail.com>
2023-12-22 18:23:41 +0400

    chore: setup shadcn/ui

--- src/main.tsx
+++ src/main.tsx
@@ -1,23 +1,24 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
 
-import FriendsListPage from './features/friends/pages/list.tsx'
-import App from './App.tsx'
+import FriendsListPage from "./features/friends/pages/list.tsx";
+import App from "./App.tsx";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <App />
+    element: <App />,
   },
   {
     path: "/friends",
-    element: <FriendsListPage />
-  }
-])
+    element: <FriendsListPage />,
+  },
+]);
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <RouterProvider router={router} />
   </React.StrictMode>,
-)
+);
